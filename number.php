55c8b6fca6deb3d80d8544f4c14c6ff3adbf7690
Kovpak81 <gorodko5@mail.ru>
2022-09-03 19:51:03 +1000

    готово

---
+++ number.php
@@ -0,0 +1,21 @@
+<?php 
+$a = 0;
+$b = 0;
+$b2 = 1;
+$a2 = 1;
+$true = true;
+$false = false;
+$str = "1";
+$string = "php";
+$null = null;
+?>
+
+
+
+<?php
+$name = 'Александр';
+$surname = 'Иванов';
+$city = 'Москва';
+$age = 28;
+?>
+
